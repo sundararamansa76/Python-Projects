9ef4ddf3b35a9fdc394c3d4d67df134425b111f5
Sundararaman S A <sundararamansa76@gmail.com>
2020-01-29 15:17:41 +0530

    2020 Jan 29

---
+++ CoreProj/division.py
@@ -0,0 +1,10 @@
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+result1 = a // b
+result2 = a /b
+print(result1)
+print(result2)
---
+++ CoreProj/operations.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+result1 = a + b
+result2 = a - b
+result3 = a * b
+
+print(result1)
+print(result2)
+print(result3)
